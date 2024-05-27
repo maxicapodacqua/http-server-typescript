25ddb57ab78d1fe2cada2a4d009e5f72236f8f85
Maxi Capodacqua <kapomaxi1@gmail.com>
2024-05-26 21:13:35 -0500

    Reading request URL (target)

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,30 @@
 import * as net from 'net';
 
+
+function readRequest(socket:net.Socket ) {
+    socket.on('data', (data) => {
+        const input = data.toString()
+        console.log(`request: ${input}`);
+
+        const [requestLine, header] = input.split('\r\n');
+        
+        const [method, target, protocol] = requestLine.split(' ');
+
+        const isRoot = target === '' || target === '/';
+        if (method === 'GET' && isRoot) {
+            socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
+        } else {
+            socket.write(`HTTP/1.1 404 Not Found\r\n\r\n`);
+        }
+
+
+        socket.end();
+    })
+}
+
+
 const server = net.createServer((socket) => {
-    socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
-    socket.end();
+    readRequest(socket);
 });
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
