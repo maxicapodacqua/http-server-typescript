7045c2e42280fdc9f466c7b1b17ca77dcae05fc9
Maxi Capodacqua <kapomaxi1@gmail.com>
2024-05-29 20:42:46 -0500

    Allowing multiple encoding

--- app/routes/echo.ts
+++ app/routes/echo.ts
@@ -4,18 +4,16 @@ const validEncodings = ["gzip"];
 export function echo(socket: net.Socket, target: string, headers: string[]) {
   const responseBody = target.slice("/echo/".length);
 
-  let contentEncodingHeader ='';
+  let contentEncodingHeader = "";
   for (const headerLine of headers) {
-      const [name, value] = headerLine.split(":");
-      if (
-        name.toLowerCase() === "accept-encoding" &&
-        validEncodings.includes(value.trim().toLowerCase())
-      ) {
-        contentEncodingHeader = `Content-Encoding:  ${value.trim()}\r\n`;
-      }
+    const [name, rawValues] = headerLine.split(":");
+    const value = rawValues
+      .split(",")
+      .find((v) => validEncodings.includes(v.trim()));
+    if (name.toLowerCase() === "accept-encoding" && value !== undefined) {
+      contentEncodingHeader = `Content-Encoding:  ${value.trim()}\r\n`;
+    }
   }
-
-  
 
   socket.write(
     `HTTP/1.1 200 OK\r\n${contentEncodingHeader}Content-Type: text/plain\r\nContent-Length: ${responseBody.length}\r\n\r\n${responseBody}`
